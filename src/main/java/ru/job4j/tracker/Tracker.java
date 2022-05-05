61fa449a7a83a9113c83e6c429e243f06547a651
opossum <ryabkoff@rambler.ru>
2022-05-05 11:38:56 +0500

    2.5. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,12 +47,12 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
+        boolean result = index != -1;
+        if (result) {
             item.setId(id);
             items[index] = item;
         }
-        return rsl;
+        return result;
     }
 
     public boolean delete(int id) {
