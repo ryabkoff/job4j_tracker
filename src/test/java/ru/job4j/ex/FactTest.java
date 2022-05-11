55da3c88e8a17b724e61379f9b96af820dee09b0
opossum <ryabkoff@rambler.ru>
2022-05-11 15:28:09 +0500

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNLessThen0() {
+        new Fact().calc(-2);
+    }
+}
