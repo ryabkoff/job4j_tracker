0e1d450d5da5d1806783608a7960cea312ca8bd1
opossum <ryabkoff@rambler.ru>
2022-05-18 14:42:21 +0500

    4. Сортировка номера [#221284 #302234]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        return Integer.compare(Integer.parseInt(left.split(". Task.")[0]),
-                Integer.parseInt(right.split(". Task.")[0]));
+        return Integer.compare(Integer.parseInt(left.split("\\.")[0]),
+                Integer.parseInt(right.split("\\.")[0]));
     }
 }
