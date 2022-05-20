01880c9625da62ae15953c34c81f303f1e8c4ed3
opossum <ryabkoff@rambler.ru>
2022-05-20 18:13:40 +0500

    2. Отсортировать департаменты [#34352 #302643]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,16 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int o1SlashPos = o1.indexOf("/");
-        int o2SlashPos = o2.indexOf("/");
-        String firstPartO1 = o1SlashPos > 0 ? o1.substring(0, o1SlashPos) : o1;
-        String secondPartO1 = o1SlashPos > 0 ? o1.substring(o1SlashPos + 1) : "";
-        String firstPartO2 = o2SlashPos > 0 ? o2.substring(0, o2SlashPos) : o2;
-        String secondPartO2 = o2SlashPos > 0 ? o2.substring(o2SlashPos + 1) : "";
-        int rsl = firstPartO2.compareTo(firstPartO1);
-        if (rsl == 0) {
-            rsl = secondPartO1.compareTo(secondPartO2);
-        }
-        return rsl;
+        String[] first = o1.split("/");
+        String[] second = o2.split("/");
+        int res = second[0].compareTo(first[0]);
+        return res == 0 ? o1.compareTo(o2) : res;
     }
 }
