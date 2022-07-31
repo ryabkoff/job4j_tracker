d693fcb6fd90fbc519f22a7447ec8c2054793f29
opossum <ryabkoff@rambler.ru>
2022-07-31 13:56:43 +0500

    2. Найти утечку памяти. [#504882]

---
+++ src/main/java/ru/job4j/gc/leak2/Post.java
@@ -0,0 +1,31 @@
+package ru.job4j.gc.leak2;
+
+import java.util.List;
+
+public class Post {
+
+    private Integer id;
+
+    private String text;
+
+    private List<Comment> comments;
+
+    public Post(Integer id, String text, List<Comment> comments) {
+        this.id = id;
+        this.text = text;
+        this.comments = comments;
+    }
+
+    public Post(String text, List<Comment> comments) {
+        this.text = text;
+        this.comments = comments;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
